ca155ffaa53e49cfafb975d363cc47315764a973
shubhankarnikam-45 <shubhankarnikam45@gmail.com>
2025-07-09 19:55:06 +0530

    adding the code of calculator in GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react'
+import "./App.css"
+import { getValue } from '@testing-library/user-event/dist/utils';
+const App = () => {
+
+  //created the state variable for the storing the input value on button click.
+  const[input, setInput] = useState('');
+
+  //calculate value when use click on the '=' button.
+  const calculateValue=(value)=>
+  {
+
+    const operators = ['+', '-', '/', '*', '%'];
+
+    //runngng for loop on the value string and find the operator.
+    let operator = null;
+    for(let i=0; i<value.length;i++)
+    {
+      if(operators.includes(value[i]))
+      {
+        operator = value[i];
+        break;
+      }
+    }
+
+    if(!null){
+      setInput(value);
+      return;
+    }
+
+
+ 
+    const[operand1, operand2] = value.split(operator).map(parseFloat);
+
+
+    //use swithch case to find the result.
+    let result = -1;
+
+    // console.log(operand1, operand2,operator)
+
+    switch(operator)
+    {
+      case '+':
+        setInput(operand1 + operand2);
+        break;
+      case '-':
+        setInput(operand1 - operand2);
+        break;
+      case '/':
+        setInput(operand1 / operand2);
+        break;
+      case '*':
+        setInput(operand1 * operand2);
+        break;
+      case '%':
+        setInput(operand1 % operand2);
+        break;
+      default : 
+        setInput("");
+        break;
+    }
+    
+  }
+  //created the handleClick function.
+  const handleClick = (inputData)=>{
+
+    if(inputData === 'C')
+    {
+      setInput("");
+    }
+    else if(inputData === '<')
+    {
+      setInput(input.slice(0, -1));
+    }
+    else if(inputData === '=')
+    {
+      calculateValue(input);
+    }
+    else {
+      setInput((prev)=>prev + inputData)
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='calc'>
+        <h1 id='input'>{input}</h1>
+
+        <div>
+          <button onClick={() => handleClick('C')}>C</button>
+          <button onClick={() => handleClick('<')}>&lt;</button>
+          <button onClick={() => handleClick('%')}>%</button>
+          <button onClick={() => handleClick('/')}>/</button>
+        </div>
+
+        <div>
+          <button onClick={() => handleClick('7')}>7</button>
+          <button onClick={() => handleClick('8')}>8</button>
+          <button onClick={() => handleClick('9')}>9</button>
+          <button onClick={() => handleClick('*')}>*</button>
+        </div>
+
+        <div>
+          <button onClick={() => handleClick('4')}>4</button>
+          <button onClick={() => handleClick('5')}>5</button>
+          <button onClick={() => handleClick('6')}>6</button>
+          <button onClick={() => handleClick('-')}>-</button>
+        </div>
+
+        <div>
+          <button onClick={() => handleClick('1')}>1</button>
+          <button onClick={() => handleClick('2')}>2</button>
+          <button onClick={() => handleClick('3')}>3</button>
+          <button onClick={() => handleClick('+')}>+</button>
+        </div>
+
+        <div>
+          <button onClick={() => handleClick('0')}>0</button>
+          <button onClick={() => handleClick('00')}>00</button>
+          <button onClick={() => handleClick('.')}>.</button>
+          <button onClick={() => handleClick('=')}>=</button>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
